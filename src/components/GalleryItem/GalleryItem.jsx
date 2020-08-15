9b1286c3a3ae45c5454bb6e2d6b192bee8816d49
sukhanova <verusia.s@gmail.com>
2020-08-15 15:48:50 -0500

    added functionality to flip photo on click

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
 
 class GalleryItem extends Component { 
+
+    state = {
+        showPhoto: true,
+    }
+
+    handleClick = () => { 
+        this.setState({
+            showPhoto: !this.state.showPhoto,
+        })
+    }
+
     render() { 
         return (
-            <div>
-                <p><img src={this.props.photo.path} /></p>
-                <p>{this.props.photo.description}</p>
-                <button onClick={(event) => this.props.likePhotoButton(this.props.photo.id)}>I Like It!</button>
+            <div onClick={this.handleClick} className="individualPhoto">
+                {this.state.showPhoto ?
+                    <img
+                        key={this.props.photo.id}
+                        src={this.props.photo.path}
+                        alt={this.props.photo.description} />
+                    :
+                    <div>
+                        <p>{this.props.photo.description}</p>
+                    </div>
+                }
+                    <button onClick={(event) => this.props.likePhotoButton(this.props.photo.id)}>I Like It!</button>
+                
                 <span>
                     {this.props.photo.likes === 0 ?
                         <p>No people love this :(</p>
