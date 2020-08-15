30b4c6b9361ebcaa920818cf3b26158e9a9b016f
sukhanova <verusia.s@gmail.com>
2020-08-14 19:52:56 -0500

    added description and Like button to dispay on dom

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -4,7 +4,9 @@ class GalleryItem extends Component {
     render() { 
         return (
             <div>
-                <p><img src={this.props.photos.path}></img></p>
+                <p><img src={this.props.photos.path} /></p>
+                <p>{this.props.photos.description}</p>
+                <button>I Like It!</button>
             </div>
         )
     }
