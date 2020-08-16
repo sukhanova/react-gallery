d298f03db833951ccc929d6d8acede63939e8c5f
sukhanova <verusia.s@gmail.com>
2020-08-16 09:23:24 -0500

    fixed issue with photo flippen when Like button clicked

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -14,40 +14,15 @@ class GalleryItem extends Component {
 
     render() { 
         return (
-          <div
-            onClick={this.handleClick}
-
-          >
-            {this.state.showPhoto ? (
-              <img
-                key={this.props.photo.id}
-                src={this.props.photo.path}
-                alt={this.props.photo.description}
-              />
-            ) : (
-              <div>
-                <p>{this.props.photo.description}</p>
-              </div>
-            )}
-            <br />
-            <div>
-              <button
-                onClick={(event) =>
-                  this.props.likePhotoButton(this.props.photo.id)
-                }
-              >
-                I Like It!
-              </button>
-              <span>
-                {this.props.photo.likes === 0 ? (
-                  <p>No people love this :(</p>
-                ) : (
-                  <p>{this.props.photo.likes} people love this!</p>
-                )}
-              </span>
-            </div>
+          <div>
+            {this.state.showPhoto ?
+              <img src={this.props.photo.path} onClick={this.handleClick} alt={this.props.photo.description} />
+              : <div onClick={this.handleClick}>
+                <p>{this.props.photo.description}</p></div>}
+            <p>{this.props.photo.likes} people like it!</p>
+            <button onClick={(event) => this.props.likePhotoButton(this.props.photo)}>I LIKE IT!</button>
           </div>
-        );
+        )
     }
 }
 
